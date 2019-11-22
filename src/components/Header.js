4aa8cca84591abcfd398e5cab0f25b075949885d
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-21 19:59:53 -0700

    changes 11-21

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../assets/926 FSS Patch.png'
+import { Link } from 'react-router-dom'
 
 
 function Header() {
@@ -14,8 +15,14 @@ function Header() {
                     <h3 className="title">926 FSS Sandstormer Booster Club</h3>
                 </div>
                 <div className="icon_holder" >
-                    <i className="fas fa-align-justify fa-2x"></i>
+                    <i className="fas fa-align-justify"></i>
                 </div>
+               <li className="links">
+                    <li><Link to='/'><button>Main</button></Link></li>
+                    <li><Link to='/Auth'><button>Login/Register</button></Link></li>
+                    <li><Link to='/Payment'><button>Donate</button></Link></li>
+                    <li><Link to='/Admin'><button>Admin</button></Link></li>
+               </li>
             </div>
         </header>
     )
