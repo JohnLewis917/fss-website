78d582095974eda8fcfd69050cbd3fe8d47e73f4
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-23 13:56:24 -0700

    changes 11-23

--- src/components/EventItems.js
+++ src/components/EventItems.js
@@ -1,30 +1,39 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 class EventItems extends Component {
-    constructor(){
-        super()
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return(
-            
-            <div className="container-2">
-                <div>
-                    <h6>
-                        {this.props.eventListObj.date}
-                    </h6>
-                    <h6>
-                        {this.props.eventListObj.event}
-                    </h6>
-                    <h6>
-                        {this.props.eventListObj.description}
-                    </h6>
-                         
-                </div>
-                    
-            </div>
-            
-        )
-    }
+  render() {
+    return (
+      <div className="container-2">
+        <table className="pure-table pure-table-bordered">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Event</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <h6>{this.props.eventListObj.date}</h6>
+              </td>
+              <td>
+                <h6>{this.props.eventListObj.event}</h6>
+              </td>
+              <td>
+                <h6>{this.props.eventListObj.description}</h6>
+              </td>
+            </tr>
+            <button>Edit</button>
+            <button>Delete</button>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-export default EventItems
+export default EventItems;
