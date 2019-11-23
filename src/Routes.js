8b10fa4caff0a4524fc4937f0e02de19eb1016a9
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-22 19:30:53 -0700

    changes 11-22

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,16 @@
-import {HashRouter,Route} from 'react-router-dom'
+import {Switch,Route} from 'react-router-dom'
+import React from 'react'
 import Dashboard from './components/Dashboard'
 import Auth from './components/Auth'
-import Payment from './components/Payment'
+// import Payment from './components/Payment'
 import Admin from './components/Admin'
 
+export default (
 
- <Route exact path='/' component={Dashboard}/>
- <Route path='/Auth' component={Auth}/>
- <Route path='/Payment' component={Payment}/>
- <Route path='/Admin' component={Admin}/>
+    <Switch>
+        <Route exact path='/' component={Dashboard}/>
+        <Route path='/Auth' component={Auth}/>
+        {/* <Route path='/Payment' component={Payment}/> */}
+        <Route path='/Admin' component={Admin}/>
+    </Switch>
+)
