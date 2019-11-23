f6343775c1f81c3c84c2c93d912123e2cc52502e
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-22 19:29:35 -0700

    changes 11-22

---
+++ src/components/Dashboard.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+class Dashboard extends Component {
+    constructor (){
+        super ()
+
+        this.state = {
+            officer_list: [],
+            events_list: []
+        }
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Dashboard
