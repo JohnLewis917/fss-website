d4ad1a36410ae91eb1dc92c9edeb8cfd3f3e11ae
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-24 15:10:27 -0700

    changes 11-24

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react'
+import { FilePond, registerPlugin } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+registerPlugin(FilePondPluginImagePreview);
 
 class Dashboard extends Component {
     constructor (){
@@ -9,10 +14,14 @@ class Dashboard extends Component {
             events_list: []
         }
     }
+    
     render(){
         return(
             <div>
-                
+                <div className="file-pond">
+
+                <FilePond allowMultiple={true} />
+                </div>
             </div>
         )
     }
