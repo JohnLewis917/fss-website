02166749c43346fd116ba545f3f662d611efe9a8
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-23 21:48:20 -0700

    changes 11-23

--- src/components/Member.js
+++ src/components/Member.js
@@ -7,16 +7,31 @@ class Member extends Component {
   render() {
     return (
       <div>
-        <div className="container-3">
-          <div>
-            <h6>{this.props.memberListObj.rank}</h6>
-            <h6>{this.props.memberListObj.first_name}</h6>
-            <h6>{this.props.memberListObj.last_name}</h6>
-            <h6>{this.props.memberListObj.email}</h6>
-            <h6>{this.props.memberListObj.paid}</h6>
-            <h6>{this.props.memberListObj.amount}</h6>
-            <h6>{this.props.memberListObj.isAdmin}</h6>
-          </div>
+        <div>
+          <h6>{this.props.memberListObj.rank}</h6>
+        </div>
+        <div>
+          <h6>{this.props.memberListObj.first_name}</h6>
+        </div>
+        <div>
+          <h6>{this.props.memberListObj.last_name}</h6>
+        </div>
+        <div>
+          <h6>{this.props.memberListObj.email}</h6>
+        </div>
+        <div>
+          <h6>{this.props.memberListObj.paid}</h6>
+        </div>
+        <div>
+          <h6>{this.props.memberListObj.amount}</h6>
+        </div>
+        <div>
+          <h6>{this.props.memberListObj.isAdmin}</h6>
+        </div>
+
+        <div>
+          <button>Edit</button>
+          <button>Delete</button>
         </div>
       </div>
     );
