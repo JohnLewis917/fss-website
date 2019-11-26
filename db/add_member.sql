8e0fc112c886d81d7cb2b2a79117b5fd99870a17
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-26 16:30:11 -0700

    changes 11-26

--- db/add_member.sql
+++ db/add_member.sql
@@ -1,2 +1,4 @@
-insert into member_list(member_rank, first_name, last_name, email);
+insert into member_list(member_rank, first_name, last_name, email)
 values(${member_rank}, ${first_name}, ${last_name}, ${email})
+
+returning *
