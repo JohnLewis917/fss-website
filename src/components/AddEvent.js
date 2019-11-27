eb822b4751f8440b69367e58c9b1e7c8438e67eb
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-26 19:58:55 -0700

    changes 11-26

---
+++ src/components/AddEvent.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+class AddEvent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: "",
+      event: "",
+      description: ""
+    };
+    this.handleDate = this.handleDate.bind(this);
+    this.handleEvent = this.handleEvent.bind(this);
+    this.handleDesc = this.handleDesc.bind(this);
+    this.addEvent = this.addEvent.bind(this);
+  }
+  handleDate(event) {
+    this.setState({ date: event.target.value });
+    console.log(event.target.value);
+  }
+  handleEvent(event) {
+    this.setState({ event: event.target.value });
+  }
+  handleDesc(event) {
+    this.setState({ description: event.target.value });
+  }
+  addEvent(event) {
+    event.preventDefault();
+    this.props.onAddEvent(this.state);
+    this.setState({ date: "", event: "", description: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="event-title">Add Event</h1>
+
+        <form className="add-event" onSubmit={this.addEvent}>
+          <h5>Date</h5>
+          <input
+            className="input-1"
+            name="Date"
+            value={this.state.date}
+            type="text"
+            size="10"
+            onChange={this.handleDate}
+          ></input>
+          <br></br>
+          <h5>Event</h5>
+          <input
+            className="input-2"
+            name="Event"
+            value={this.state.event}
+            type="text"
+            size="35"
+            onChange={this.handleEvent}
+          ></input>
+          <br></br>
+          <h5>Description</h5>
+          <textarea
+            className="input-3"
+            name="description"
+            value={this.state.description}
+            type="text"
+            size="45"
+            onChange={this.handleDesc}
+          ></textarea>
+          <br></br>
+          <br></br>
+          <button color="primary" className="submitEvent" type="submit">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+export default AddEvent;
