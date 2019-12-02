843f6d0fb7185811770f4cbef82f6a92c660edbb
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-02 16:38:35 -0700

    changes 12-2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {HashRouter} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./ducks/store";
 // import './main.scss';
 
 ReactDOM.render(
+  <Provider store={store}>
     <HashRouter>
-       <App />
-    </HashRouter>,
-document.getElementById('root'));
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
