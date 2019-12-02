329d0d81d931f180a69c4c67ba1287f4f6e41288
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-02 16:38:14 -0700

    changes 12-2

--- src/Routes.js
+++ src/Routes.js
@@ -2,17 +2,18 @@ import {Switch,Route} from 'react-router-dom'
 import React from 'react'
 import Dashboard from './components/Dashboard'
 import Auth from './components/Auth'
-// import Payment from './components/Payment'
+import CheckoutForm from './components/CheckoutForm'
 import Admin from './components/Admin'
 import Login from './components/Login'
 import PrivateRoute from 'react-private-route'
+import Stripe from './components/Stripe'
 
 export default (
 
     <Switch>
         <Route exact path='/' component={Dashboard}/>
         <Route path='/Auth' component={Auth}/>
-        {/* <Route path='/Payment' component={Payment}/> */}
+        <Route path='/Payment' component={Stripe}/>
         <Route path='/Admin' component={Admin}/>
         {/* <PrivateRoute path='/Admin' component={Admin} isAdmin/> */}
         <Route path='/Login' component={Login}/>
