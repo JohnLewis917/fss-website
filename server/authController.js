61b6d95ac914b67f6c36237bbc251b7aec834fca
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-02 16:38:56 -0700

    changes 12-2

--- server/authController.js
+++ server/authController.js
@@ -11,7 +11,7 @@ module.exports = {
       const member_id = await db.add_member({member_rank, first_name, last_name, email})
       const salt = bcrypt.genSaltSync(10)
       const hash = bcrypt.hashSync(password, salt)
-      db.add_hash({member_id: member_id[0].member_id, hash})
+      db.add_hash({member_id: member_id[0].id, hash})
       req.session.member = {member_id: member_id[0].member_id, member_rank, first_name, last_name, email}
       res.status(201).send({message: 'Logged In', member: req.session.member})
     },
