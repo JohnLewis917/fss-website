dd538e15355a76c14a46efc520d69988a0c27e3e
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-02 16:36:40 -0700

    changes 12-2

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,7 +1,35 @@
+
 const initialState = {
     member_rank: "",
     first_name: "",
     last_name: "",
-    image:""
+    image:"",
+    date:"",
+    event:"",
+    description:""
+}
+GET_EVENTS = 'GET_EVENTS'
+GET_OFFICERS = 'GET_OFFICERS'
+
+export const getEvents = (date, event, description) => {
+    return {
+        type: GET_EVENTS,
+        payload: {date, event, description}
+    }
+}
+export const getOfficers = (member_rank, first_name, last_name, image) => {
+    return {
+        type: GET_OFFICERS,
+        payload: {member_rank, first_name, last_name, image}
+    }
 }
 
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        case GET_EVENTS:
+            return {...state, ...action.payload}
+        case GET_OFFICERS:
+            return {...state, ...action.payload}
+        default: return state
+    }
+}
