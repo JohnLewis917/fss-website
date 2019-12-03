8a18dab72dc25220aee6a579f65f10561240db58
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-02 19:51:18 -0700

    changes 12-2

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { HashRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./ducks/store";
+// import { Provider } from "react-redux";
+// import store from "./ducks/store";
 // import './main.scss';
 
 ReactDOM.render(
-  <Provider store={store}>
+//   <Provider store={store}>
     <HashRouter>
       <App />
     </HashRouter>
-  </Provider>,
-  document.getElementById("root")
+//   </Provider>,
+  ,document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
