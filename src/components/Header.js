9e676ef22e0e579512ba36c3428cc8ed7a73800f
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-02 19:50:40 -0700

    changes 12-2

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function Header() {
         <div >
           <div className="icon_holder">
             <a
-            //   onClick={myFunction()}
+              onClick={() => {myFunction()}}
               className="fas fa-align-justify"
               href="#"
               data-target="dropdown1"
@@ -57,23 +57,23 @@ function Header() {
       </div>
     </header>
   );
-// }
-// function myFunction() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// }
+}
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-// // Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches(".fa-align-justify")) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains("show")) {
-//         openDropdown.classList.remove("show");
-//       }
-//     }
-//   }
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".fa-align-justify")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
 };
 
 export default Header;
