79117e79a4f01be529b281175e65bcd36951d559
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-03 19:39:37 -0700

    changes 12-3

---
+++ src/components/Officers.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+// import {connect} from 'react-redux'
+
+
+class Officers extends Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+
+            <div>
+                <input type="image" id="image" src="" alt=""></input>
+                <input first_name="text" size="35" type="text"></input>
+                <input last_name="text" size="35" type="text"></input>
+                
+            </div>
+        )
+    }
+}
+export default Officers 
