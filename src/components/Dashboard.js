929d290ce87e135cb947c5cf2464c934cfdfe1ca
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-06 07:31:11 -0700

    changes 12-5

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,30 +1,49 @@
-import React, {Component} from 'react'
-import Officers from './Officers'
-import axios from 'axios'
-
+import React, { Component } from "react";
+import { getEvents } from "../ducks/actions";
+import { connect } from "react-redux";
+import EventItem from "./EventItem";
+import Officers from "./Officers";
 
 class Dashboard extends Component {
-    constructor (){
-        super ()
+  constructor() {
+    super();
+  }
+  componentDidMount() {
+    this.props.getEvents();
+  }
 
-        this.state = {
-            officerList: [],
-            eventList: []
-        }
-    }
-    
-    
-    render(){
-        return(
-            <div>
-                <div>
-                
-                
-                </div>
-            </div>
-        )
-    }
+  render() {
+    console.log(this.props.eventList);
+    return (
+      <div className="flex">
+        <div>
+        <h2>Upcoming Events</h2>
+          <table className="table-1">
+            <th>Date</th>
+            <th>Event</th>
+            <th>Description</th>
+            {this.props.eventList.map(el => (
+              <tr>
+              <td>{el.date}</td>
+              <td>{el.event}</td>
+              <td>{el.description}</td>
+      
+              
+            </tr>
+            ))}
+          </table>
+        </div>
+        <div>
+          <Officers/>
+        </div>
+      </div>
+    );
+  }
 }
+const mapStateToProps = state => {
+  return {
+    eventList: state.events
+  };
+};
 
-
-export default Dashboard
+export default connect(mapStateToProps, { getEvents })(Dashboard);
