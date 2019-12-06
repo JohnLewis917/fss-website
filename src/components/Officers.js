92c41dce110eab5d480f4c73325eb49729fedc68
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-06 07:30:36 -0700

    changes 12-5

--- src/components/Officers.js
+++ src/components/Officers.js
@@ -5,13 +5,18 @@ import React, {Component} from 'react'
 class Officers extends Component{
     constructor(){
         super()
+        this.state = {
+            image: "",
+            
+        }
     }
 
     render(){
         return(
 
             <div>
-                <input type="image" id="image" src="" alt=""></input>
+                <img src={this.state.image}/>
+                <input value={this.state.image} onChange={(event) => this.setState({image:event.target.value})}></input>
                 <input first_name="text" size="35" type="text"></input>
                 <input last_name="text" size="35" type="text"></input>
                 
