653378a1da0a7bbb1d340658cf4ef6f37f4d0969
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-06 07:29:37 -0700

    changes 12-5

--- src/ducks/types.js
+++ src/ducks/types.js
@@ -2,3 +2,7 @@ export const GET_EVENTS = 'GET_EVENTS'
 export const ADD_EVENT = 'ADD_EVENT'
 export const UPDATE_EVENT = 'UPDATE_EVENT'
 export const DELETE_EVENT = 'DELETE_EVENT'
+
+export const GET_OFFICERS = 'GET_OFFICERS'
+export const ADD_OFFICER = 'ADD_OFFICER'
+export const DELETE_OFFICER = 'DELETE_OFFICER'
