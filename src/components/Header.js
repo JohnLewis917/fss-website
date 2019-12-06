73947cb691cec04bc727b0d589820864481769f3
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-06 14:45:49 -0700

    changes 12-6

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,63 +1,66 @@
-import React from "react";
+import React, {Component} from "react";
 import logo from "../assets/926 FSS Patch.png";
 import { Link } from "react-router-dom";
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-function Header(props) {
-  const logout = () => {
-    axios.delete('/auth/logout').then(res => {
-      Swal.fire(res.data.message)
-      // props.updateUserInfo({
-      //   email: '',
-      //   name: '',
-      //   user_id: '',
-      //   profile_img: ''
-      // })
-    })
+class Header extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {}
+    }
   }
-  return (
-    <header>
-      <div className="heading">
-        <div className="logo-holder">
-          <img className="logo" src={logo} alt="logo" />
-        </div>
-        <div className="title-holder">
-          <h3 className="title">926 FSS Sandstormer Booster Club</h3>
-        </div>
-        <div>
-          <div className="icon_holder">
-            <i className="fas fa-align-justify"></i>
+  async logout() {
+    axios.delete('/auth/logout')
+    this.setState({ user: {} })
+  }
+  render(){
+
+    return (
+      <header>
+        <div className="heading">
+          <div className="logo-holder">
+            <img className="logo" src={logo} alt="logo" />
           </div>
+          <div className="title-holder">
+            <h3 className="title">926 FSS Sandstormer Booster Club</h3>
+          </div>
+          <div>
+            <div className="icon_holder">
+              <i className="fas fa-align-justify"></i>
+            </div>
+          </div>
+          <li className="links">
+            <li>
+              <Link to="/">
+                <button>Main</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Payment">
+                <button>Donate</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Admin">
+                <button>Admin</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Auth">
+                <button>Login/Register</button>
+              </Link>
+            </li>
+            <li>
+              <button onClick={() => this.logout()}>Logout</button>
+            </li>
+          </li>
         </div>
-        <li className="links">
-          <li>
-            <Link to="/">
-              <button>Main</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Payment">
-              <button>Donate</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Admin">
-              <button>Admin</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/Auth">
-              <button>Login/Register</button>
-            </Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </li>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  }
 }
+  
 
 export default Header;
