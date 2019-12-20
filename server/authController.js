f44b2d046dc95ff4debafbd0850fff69d2ac59c1
John Lewis <johnlewis@MacBook-Pro.local>
2019-12-19 19:37:34 -0700

    changes 12-19

--- server/authController.js
+++ server/authController.js
@@ -9,10 +9,12 @@ module.exports = {
         return res.status(409).send({message: 'Email already registered'})
       }
       const member_id = await db.add_member({member_rank, first_name, last_name, email, password})
+      
       const salt = bcrypt.genSaltSync(10)
       const hash = bcrypt.hashSync(password, salt)
       db.add_hash({member_id: member_id[0].id, hash})
-      req.session.member = {member_id: member_id[0].member_id, member_rank, first_name, last_name, email}
+      req.session.member = {member_id: member_id[0].id, member_rank, first_name, last_name, email}
+      
       res.status(201).send({message: 'Logged In', member: req.session.member})
     },
     login: async (req, res) => {
@@ -23,13 +25,15 @@ module.exports = {
         return res.status(401).send({message: 'An account with that email does not exist'})
       }
       const foundMember = await db.find_hash([email])
-      const {hash, member_id, member_rank, first_name, last_name} = foundMember[0]
+      console.log(foundMember)
+      const {hash, member_id, member_rank, first_name, last_name, isadmin} = foundMember[0]
       const result = bcrypt.compareSync(password, hash)
       if (!result) {
         return res.status(401).send({message: 'Password incorrect'})
       }else {
-        req.session.user = {member_id, member_rank, first_name, last_name, email}
-        res.status(200).send({message: 'Logged In', member: req.session.member})
+        req.session.user = {member_id, member_rank, first_name, last_name, email,isadmin}
+        
+        res.status(200).send({message: 'Logged In', member: req.session.user})
       }
     },
     logout: (req, res) => {
